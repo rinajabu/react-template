28a81da618f0ba9803a90b90579e770a9bda535f
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro-2.local>
2022-01-09 20:17:12 -0500

    top nav component created, react router set up

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './sass/App.scss';
 import Home from './components/Home/Home';
+import TopNav from './components/TopNav/TopNav';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <TopNav />
         <Routes>
           <Route exact path="/" element={<Home />} />
         </Routes>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,8 +4,9 @@ import styles from './home.module.scss'
 const Home = () => {
     return (
         <div className={styles.home}>
+            <h1>React Template</h1>
             <p>
-                React template with react-router, protected routes, context, bootstrap, and SASS.
+                With react-router, protected routes, context, bootstrap, and SASS.
             </p>
         </div>
     )
