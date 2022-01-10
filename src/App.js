33b4da61d5abe1a0c6dc58e6ab656c64fbd15a70
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro-2.local>
2022-01-09 20:06:19 -0500

    home component created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from './assets/logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './sass/App.scss';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React template with react-router, protected routes, context, bootstrap, and SASS.
-        </p>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './home.module.scss'
+
+const Home = () => {
+    return (
+        <div className={styles.home}>
+            <p>
+                React template with react-router, protected routes, context, bootstrap, and SASS.
+            </p>
+        </div>
+    )
+}
+
+export default Home
